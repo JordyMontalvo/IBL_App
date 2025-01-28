f8ed02190c36ef841a782c886a772d4ad16b8de1
JordyMontalvo <jordyjosephmontalvo@gmail.com.com>
2025-01-28 13:02:54 -0500

    cambios en la vista  welcome

--- src/views/auth/Welcome.vue
+++ src/views/auth/Welcome.vue
@@ -1,25 +1,30 @@
 <template>
-  <Auth> <br>
-
-    <router-link to="/login"    class="button large">INGRESAR A OFICINA</router-link>
-    <router-link to="/register" class="button large">REGISTRAR DISTRIBUIDOR</router-link>
-    <!-- <router-link to="/welcome"  class="button large">ACESSO</router-link>
-    <router-link to="/welcome"  class="button large">APP</router-link> -->
-
-    <a :href="wsp"  target="_blank" class="route">WhatsApp de ayuda</a>
-
+  <Auth>
+    <section class="welcome-container">
+      <div class="logo-container2">
+         <img src="@/assets/img/logo/logoibl.png" alt="Logo IBL">
+      </div>
+      
+      <router-link to="/login" class="button">Ingresar a Oficina</router-link>
+      <router-link to="/register" class="button">Registrar Distribuidor</router-link>
+      
+      <a :href="wsp" target="_blank" class="whatsapp-link">
+        <i class="fa-brands fa-whatsapp"></i>
+        WhatsApp de ayuda
+      </a>
+    </section>
   </Auth>
 </template>
 
 <script>
 import Auth from '@/views/layouts/Auth'
+import '@/assets/style/welcome.styl'
 
 export default {
   components: { Auth },
-
   computed: {
-    // wsp()  { return this.$store.state.wsp_ec }
-    wsp()  { return this.$store.state.wsp_pe }
+    wsp() { return this.$store.state.wsp_pe }
   },
 }
 </script>
+
