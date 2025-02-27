0503fcf71b581d9df87881f5d8c8cfb1090dcc9f
JordyMontalvo <jordyjosephmontalvo@gmail.com.com>
2025-02-27 14:24:19 -0500

    changes diseño

--- src/views/app/Activation.vue
+++ src/views/app/Activation.vue
@@ -8,214 +8,232 @@
       </router-link>
     </h4>
 
-
     <i class="load" v-if="loading"></i>
 
     <section v-if="!loading">
       <div class="flex">
-        <div class = "flex-activacion">
-        <div style="width: 100%">
-          <div style="display: flex; gap: 10px">
-            <button
-              class="_tabs"
+        <div class="flex-activacion">
+          <div style="width: 100%">
+            <div style="display: flex; gap: 10px">
+              <button
+                class="_tabs"
+                v-for="(category, i) in categories"
+                @click="tab = category"
+                :class="{
+                  selected: tab == categories[i],
+                  'not-selected': tab != categories[i],
+                }"
+              >
+                {{ category }}
+              </button>
+            </div>
+            <div
+              class="_tab_content"
+              style="color: rgba(8, 56, 92, 1)"
               v-for="(category, i) in categories"
-              @click="tab = category"
-              :class="{ selected: tab == categories[i] }"
+              v-show="tab == categories[i]"
             >
-              {{ category }}
-            </button>
-          </div>
-          <div
-            class="_tab_content"
-            style="color: rgba(8, 56, 92, 1)"
-            v-for="(category, i) in categories"
-            v-show="tab == categories[i]"
-          >
-            <article
-              class="product"
-              v-for="(product, i) in products"
-              @click="touch(i)"
-              v-if="product.type == category"
-            >
-              <small>
-                <p class="_name">{{ product.name }}</p>
-                <span style="color: rgba(8, 56, 92, 1); font-size: 14px; font-weight: bold;
-                  ">S/. {{ product.price }}</span
-                >&nbsp; &nbsp;
-                <span style="color: rgba(102, 108, 104, 1); font-weight: bold; 
-                ">PTS {{ product.points }}</span>
+              <article
+                class="product"
+                v-for="(product, i) in products"
+                @click="touch(i)"
+                v-if="product.type == category"
+              >
+                <small>
+                  <p class="_name">{{ product.name }}</p>
+                  <span
+                    style="
+                      color: rgba(8, 56, 92, 1);
+                      font-size: 14px;
+                      font-weight: bold;
+                    "
+                    >S/. {{ product.price }}</span
+                  >&nbsp; &nbsp;
+                  <span style="color: #aaa3a3; font-weight: bold"
+                    >PTS {{ product.points }}</span
+                  >
 
-                <!-- <span>Val. a comisionar
+                  <!-- <span>Val. a comisionar
                   <i v-if="product.val">{{ product.val   }}</i>
                   <i v-else>            {{ product.price }}</i>
                   &nbsp; &nbsp; <i class="_price">$ {{ product.price }}</i></span> -->
-              </small>
+                </small>
 
-              <div class="control" style="color: green">
-                <i class="fas fa-minus-square" @click="less(product)"></i>
-                <input v-model="product.total" readonly />
-                <i class="fas fa-plus-square" @click="more(product)"></i>
-              </div>
-            </article>
-          </div>
-          <br />
+                <div class="control" style="color: green">
+                  <i class="fas fa-minus-square" @click="less(product)"></i>
+                  <input v-model="product.total" readonly />
+                  <i class="fas fa-plus-square" @click="more(product)"></i>
+                </div>
+              </article>
+            </div>
+            <br />
 
-          <div>
-            <p style="color: rgba(8, 56, 92, 1); font-size: 20px">Terreno</p>
-            <img :src="product.img" style="margin: 12px 0; width: 370px ; height :170px"/>
-
-            <!-- <div class="flex"> -->
             <div>
-              <textarea
-              readonly
-                style="
-                  width: 390px;
-                  height: 100px;
-                  border-radius: 20px;
-                  padding: 10px;
-                "
-                v-model="product.description"
-              ></textarea>
+              <p style="color: rgba(8, 56, 92, 1); font-size: 18px">
+                {{ activeCategory }}
+              </p>
+              <img
+                :src="product.img"
+                style="margin: 12px 0; width: 370px; height: 170px"
+              />
+
+              <!-- <div class="flex"> -->
+              <div>
+                <textarea
+                  readonly
+                  style="
+                    width: 390px;
+                    height: 100px;
+                    border-radius: 20px;
+                    padding: 10px;
+                  "
+                  v-model="product.description"
+                ></textarea>
+              </div>
             </div>
           </div>
-        </div>
 
           <div>
-          <p style="color: rgba(74, 176, 46, 1); font-size: 24px">
-            Datos Bancarios
-          </p>
+            <p style="color: rgba(74, 176, 46, 1); font-size: 24px">
+              Datos Bancarios
+            </p>
 
-          <small style="color: rgba(8, 56, 92, 1)"
-            >Impuesto: {{ IGV.toFixed(2) }}</small
-          >
-          <br />
-
-          <div class="input-wrapper">
-            <i class="fa-solid fa-money-bill-wave icon"></i>
-            <input class="input" readonly v-model="_price" /> <br />
-          </div>
-          <div class="input-wrapper">
-            <i class="fa-solid fa-hand-holding-usd icon"></i>
-            <input class="input" readonly v-model="_points" /> <br />
-          </div>
-          <div class="input-wrapper">
-            <select class="input" v-model="office" v-if="!pending">
-              <i class="fas fa-building icon"></i>
-              <option value="null" disabled>Oficina</option>
-              <option v-for="office in offices" :value="office">
-                {{ office.name }}
-              </option>
-            </select>
+            <small style="color: rgba(8, 56, 92, 1)"
+              >Impuesto: {{ IGV.toFixed(2) }}</small
+            >
             <br />
-          </div>
-          <small v-if="office">{{ office.address }}</small> <br />
-          <div class="input-wrapper">
-            <div v-if="office">
-              <textarea
-                readonly
-                class="input"
-                style="width: 390px; height: 200px; border-radius: 20px"
-                rows="5"
-                >{{ office.accounts }}</textarea
-              >
-              <br /><br />
+
+            <div class="input-wrapper">
+              <i class="fa-solid fa-money-bill-wave icon"></i>
+              <input class="input" readonly v-model="_price" /> <br />
             </div>
-          </div>
-          <label>
-            <input type="checkbox" v-model="check" />
-            <small style="color: rgba(8, 56, 92, 1)">Deseo usar mi saldo</small>
-          </label>
-          <br />
-
-          <div v-show="check">
-            <small style="color: rgba(8, 56, 92, 1)"
-              >Saldo no disponible: {{ _balance }}</small
-            >
-            <br />
-            <small style="color: rgba(8, 56, 92, 1)"
-              >Saldo disponible: {{ balance }}</small
-            >
-            <br />
-            <small v-if="remaining > 0">restan: {{ remaining }}</small>
-          </div>
-
-          <br />
-
-          <div v-show="!(check && remaining == 0)">
-            <small style="color: rgba(74, 176, 46, 1); font-size: 24px"
-              >Medio de Pago</small
-            >
-            <br />
-
-            <div v-if="check">
-              <small v-if="plan == 'default'"
-                >Cash: {{ balance + selec_plan.pay }}</small
-              >
-              <small v-else> Cash: {{ _balance + balance }}</small>
-
+            <div class="input-wrapper">
+              <i class="fa-solid fa-hand-holding-usd icon"></i>
+              <input class="input" readonly v-model="_points" /> <br />
+            </div>
+            <div class="input-wrapper">
+              <select class="input" v-model="office" v-if="!pending">
+                <i class="fas fa-building icon"></i>
+                <option value="null" disabled>Oficina</option>
+                <option v-for="office in offices" :value="office">
+                  {{ office.name }}
+                </option>
+              </select>
               <br />
             </div>
-
+            <small v-if="office">{{ office.address }}</small> <br />
+            <div class="input-wrapper">
+              <div v-if="office">
+                <textarea
+                  readonly
+                  class="input"
+                  style="width: 390px; height: 200px; border-radius: 20px"
+                  rows="5"
+                  >{{ office.accounts }}</textarea
+                >
+                <br /><br />
+              </div>
+            </div>
             <label>
-              <input type="radio" :value="'bank'" v-model="pay_method" />
-              <small style="color: rgba(8, 56, 92, 1)">Banco</small>
+              <input type="checkbox" v-model="check" />
+              <small style="color: rgba(8, 56, 92, 1)"
+                >Deseo usar mi saldo</small
+              >
             </label>
             <br />
 
-            <label>
-              <input type="radio" :value="'cash'" v-model="pay_method" />
-              <small style="color: rgba(8, 56, 92, 1)">Efectivo</small> <br />
-            </label>
-
-            <div v-if="pay_method == 'bank'">
+            <div v-show="check">
+              <small style="color: rgba(8, 56, 92, 1)"
+                >Saldo no disponible: {{ _balance }}</small
+              >
               <br />
-              <small>Monto: {{ remaining }}</small> <br />
-
-              <input class="input" v-model="bank" placeholder="Banco" /> <br />
-              <input
-                class="input"
-                v-model="date"
-                placeholder="Fecha"
-                type="date"
-              />
+              <small style="color: rgba(8, 56, 92, 1)"
+                >Saldo disponible: {{ balance }}</small
+              >
               <br />
-              <input
-                class="input"
-                v-model="voucher_number"
-                placeholder="Número de Voucher"
-                oninput="this.value=this.value.replace(/(?![0-9])./gmi,'')"
-              />
+              <small v-if="remaining > 0">restan: {{ remaining }}</small>
+            </div>
+
+            <br />
+
+            <div v-show="!(check && remaining == 0)">
+              <small style="color: rgba(74, 176, 46, 1); font-size: 24px"
+                >Medio de Pago</small
+              >
+              <br />
+
+              <div v-if="check">
+                <small v-if="plan == 'default'"
+                  >Cash: {{ balance + selec_plan.pay }}</small
+                >
+                <small v-else> Cash: {{ _balance + balance }}</small>
+
+                <br />
+              </div>
+
+              <label>
+                <input type="radio" :value="'bank'" v-model="pay_method" />
+                <small style="color: rgba(8, 56, 92, 1)">Banco</small>
+              </label>
               <br />
 
               <label>
-                <span class="input" v-show="!voucher">Comprobante de pago</span>
-
-                <img class="voucher" :src="voucher" />
-
-                <input type="file" @change="onFileChange" />
+                <input type="radio" :value="'cash'" v-model="pay_method" />
+                <small style="color: rgba(8, 56, 92, 1)">Efectivo</small> <br />
               </label>
+
+              <div v-if="pay_method == 'bank'">
+                <br />
+                <small>Monto: {{ remaining }}</small> <br />
+
+                <input class="input" v-model="bank" placeholder="Banco" />
+                <br />
+                <input
+                  class="input"
+                  v-model="date"
+                  placeholder="Fecha"
+                  type="date"
+                />
+                <br />
+                <input
+                  class="input"
+                  v-model="voucher_number"
+                  placeholder="Número de Voucher"
+                  oninput="this.value=this.value.replace(/(?![0-9])./gmi,'')"
+                />
+                <br />
+
+                <label>
+                  <span class="input" v-show="!voucher"
+                    >Comprobante de pago</span
+                  >
+
+                  <img class="voucher" :src="voucher" />
+
+                  <input type="file" @change="onFileChange" />
+                </label>
+              </div>
+
+              <div v-if="pay_method == 'cash'">
+                <br />
+                <small>Monto: {{ remaining }}</small> <br />
+              </div>
             </div>
 
-            <div v-if="pay_method == 'cash'">
-              <br />
-              <small>Monto: {{ remaining }}</small> <br />
-            </div>
+            <small v-if="error" style="color: red"
+              ><br />
+              {{ error }}</small
+            >
+            <small v-if="success" class="success">Activación Enviada</small>
+            <br />
+
+            <button class="button" v-show="!sending" @click="POST">
+              Ordenar productos
+            </button>
+            <button class="button" v-show="sending" disabled>
+              Enviando orden ...
+            </button>
           </div>
-
-          <small v-if="error" style="color: red"
-            ><br />
-            {{ error }}</small
-          >
-          <small v-if="success" class="success">Activación Enviada</small>
-          <br />
-
-          <button class="button" v-show="!sending" @click="POST">
-            Ordenar productos
-          </button>
-          <button class="button" v-show="sending" disabled>
-            Enviando orden ...
-          </button>
-        </div>
 
           <br /><br /><br />
         </div>
@@ -323,6 +341,12 @@ export default {
 
       return ret;
     },
+
+    activeCategory() {
+      return this.tab
+        ? this.tab.charAt(0).toUpperCase() + this.tab.slice(1).toLowerCase()
+        : "Terreno"; 
+    },
   },
   async created() {
     // GET data
@@ -366,10 +390,11 @@ export default {
     },
 
     more(product) {
-    const isActivated = this.$store.state.activated || this.$store.state._activated;
+      const isActivated =
+        this.$store.state.activated || this.$store.state._activated;
 
-    // Verificar si el usuario está activado
-    /*if (isActivated) {
+      // Verificar si el usuario está activado
+      /*if (isActivated) {
       // Para usuarios activados, permitir agregar productos de cualquier tipo
       if (product.total >= 1) return;
       if(product.type === "ACTIVACIÓN" && product.total >= 1) return; 
@@ -378,9 +403,8 @@ export default {
       if (product.total >= 1) return; 
     }*/
 
-    if(this.total >=1) return;
-    product.total += 1;
-
+      if (this.total >= 1) return;
+      product.total += 1;
     },
 
     less(product) {
