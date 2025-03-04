f8c83321c57d245c76bcc5110d7cc826c5794656
JordyMontalvo <jordyjosephmontalvo@gmail.com.com>
2025-03-04 15:01:01 -0500

    cambios deslizamiento sidebar

--- src/views/layouts/App.vue
+++ src/views/layouts/App.vue
@@ -454,7 +454,7 @@ export default {
     },
     endTouch() {
       // Si el deslizamiento es hacia la izquierda y el menú está abierto, cerrarlo
-      if (this.startX - this.endX > 250 && this.open) {
+      if (this.startX - this.endX > 220 && this.open) {
         this.closeMenu();
       }
     },
